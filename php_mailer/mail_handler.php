0937cd2ab6458d910e18095491529dc4ba0f7517
vbaghdas <vbaghdas@gmail.com>
2018-02-22 18:56:25 -0800

    changed email format in php

--- php_mailer/mail_handler.php
+++ php_mailer/mail_handler.php
@@ -27,8 +27,42 @@ $mail->addReplyTo($visitor_email);
 
 $mail->isHTML(true);
 
+$message  = "<html><body>";
+$message .= "<table width='100%' bgcolor='#e0e0e0' cellpadding='0' cellspacing='0' border='0'>";
+$message .= "<tr><td>";
+$message .= "<table align='center' width='100%' border='0' cellpadding='0' cellspacing='0' style='max-width:650px; background-color:#fff; font-family:Verdana, Geneva, sans-serif;'>";
+$message .= "<thead>
+                <tr height='80'>
+                <th colspan='4' style='background-color:#7a7a7a; border-bottom: dashed #FFB6C1 2px;' >
+                    <h1 style='color: #fff'>Appointment</h1>
+                </th>
+                </tr>
+             </thead>";
+    
+$message .= "<tbody>
+                <tr>
+                <td colspan='4' style='padding:15px 30px; color: #333;'>
+                    <p style='font-size: 14px; color: #333;'>Name: ".$name."</p>
+                    <p style='font-size: 14px; color: #333;'>Time: ".$time."</p>
+                    <p style='font-size: 14px; color: #333;'>Phone:".$phone."</p>
+                    <p style='font-size: 14px; color: #333;'>Email: ".$visitor_email."</p>
+                </td>
+                </tr>
+                
+                <tr height='80'>
+                    <td colspan='4' align='center' style='background-color:#f5f5f5; border-top: dashed #FFB6C1 2px; font-size:14px;'>
+                        <label></label>
+                    </td>
+                </tr>
+            </tbody>";
+    
+$message .= "</table>";
+$message .= "</td></tr>";
+$message .= "</table>";
+$message .= "</body></html>";
+
 $mail->Subject = 'Kaliforniadoll - Appointment made by '.$name;
-$mail->Body    = $name.' wants to make an appointment on '.$time.'. Customers phone number is '.$phone;
+$mail->Body    = $message;
 
 // Send an email
 if(!$mail->send()) {
